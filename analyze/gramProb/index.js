36bcd33e6a859e3e4183203355096d1b62ef478b
ankihg <hagelin.annika@gmail.com>
2018-06-23 14:32:03 -0700

    prev gram probs

---
+++ analyze/gramProb/index.js
@@ -0,0 +1,34 @@
+'use strict';
+const process = require('../../process');
+
+module.exports = function(options, text) {
+
+    const _2prob = exec([
+        process.gram.bind(this, {n: 2}),
+        process.count.bind(this, {}),
+    ], text);
+
+    const _1prob = exec([
+        process.gram.bind(this, {n: 1}),
+        process.count.bind(this, {}),
+    ], text);
+
+    // console.log(_2prob);
+    // console.log(_1prob);
+
+    let probs = Object.keys(_2prob).reduce((acc, _2gram) => {
+        let _1grams = _2gram.split(',')
+        acc[_2gram] = _2prob[_2gram] / (_1prob[_1grams[0]] + _1prob[_1grams[1]])
+        return acc;
+    }, {});
+
+    console.log(Object.keys(probs).map((el) => { return {key: el, prob: probs[el]}}).sort((a, b) => {
+        return a.prob > b.prob ? -1 : a.prob < b.prob ? 1 : 0;
+    }));
+}
+
+
+
+function exec(fns, arg) {
+    return fns.reduce((arg, fn) => fn(arg), arg);
+}
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 const config = require('./config');
 const process = require('./process');
+const analyze = require('./analyze');
 
 const dataset = require(config.inputFilepath);
 // console.log(dataset);
@@ -11,10 +12,12 @@ const text = dataset.reduce((acc, song) => {
 
 // console.log(process.gram({n: 2}, text));
 
-const fns = [
-    process.gram.bind(this, {n: 2}),
-    process.count.bind(this, {}),
-];
+// const fns = [
+//     process.gram.bind(this, {n: 2}),
+//     process.count.bind(this, {}),
+// ];
+//
+//
+// console.log(fns.reduce((arg, fn) => fn(arg), text));
 
-
-console.log(fns.reduce((arg, fn) => fn(arg), text));
+console.log(analyze.gramProb({}, text));
